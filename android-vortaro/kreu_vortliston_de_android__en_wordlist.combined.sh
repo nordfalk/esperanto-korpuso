fd34de31580f46b9c742c936c934fc56694f1234
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2015-04-25 22:06:56 +0200

    Aldonis la plej gravajn mankantajn vortojn

--- android-vortaro/kreu_vortliston_de_android__en_wordlist.combined.sh
+++ android-vortaro/kreu_vortliston_de_android__en_wordlist.combined.sh
@@ -1,6 +1,6 @@
 echo 'dictionary=main:eo,locale=eo,description=Esperanto,date=1402373178,version=47'
 
-zcat /media/j/_data/LatinIME/dictionaries/en_wordlist.combined.gz | 
+zcat /media/j/_data/android-src/packages/inputmethods/LatinIME/dictionaries/en_wordlist.combined.gz | 
 grep ' word='  |  # nur konsideri vortojn
 perl -pe 's/ word=(.*),f=(.*)/\1,<word=\1%f=\2>/' | # formatigi al HTML
 apertium -f html-noent en-eo | # traduki al Esperanto
@@ -8,6 +8,28 @@ grep -v '^,<word=' | # forpreni HTML-ecan formaton
 grep -v '[*-]' | # forpreni vortojn nekonatajn de Apertium
 perl -pe 's/(.*?),*<word=(.*)%(.*)>/ word=\1,\3/' |
 perl -pe 's|(word=.*?) .*?,(.*)|\1,\2|' | # forpreni sekvajn vortojn se traduko donas pli ol unu
+(
+echo '
+ word=estas,f=210,flags=,originalFreq=222
+ word=ĉu,f=200,flags=,originalFreq=222
+ word=por,f=200,flags=,originalFreq=222
+ word=pri,f=200,flags=,originalFreq=222
+ word=oni,f=200,flags=,originalFreq=222
+ word=li,f=200,flags=,originalFreq=222
+ word=el,f=200,flags=,originalFreq=222
+ word=ja,f=150,flags=,originalFreq=222
+ word=kiuj,f=150,flags=,originalFreq=222
+ word=diras,f=150,flags=,originalFreq=138
+ word=da,f=150,flags=,originalFreq=222
+ word=plu,f=150,flags=,originalFreq=222
+ word=tion,f=150,flags=,originalFreq=222
+ word=per,f=150,flags=,originalFreq=222
+ word=sia,f=150,flags=,originalFreq=222
+ word=sian,f=150,flags=,originalFreq=222
+ word=iĝas,f=150,flags=,originalFreq=222
+'
+cat
+) |
 sort -u -t= -k2,2 |      # forprenu vortojn kiu aperas plurfoje - retenu la unuan kun la plej alta frekvenco
 sort -r -g -t= -k3,3    # ordigu denove laŭ frekvencoj
 
